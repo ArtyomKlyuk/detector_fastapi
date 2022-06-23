90b6e192a43165a50218d2cc54c49bf02b9d424a
ArtyomKlyuk <artyom010408@gmail.com>
2022-06-23 23:52:55 +0300

    added some function

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import os.path
+
 from fastapi import FastAPI
-import schemas
+from schemas import Detector
+
 app = FastAPI()
 
 
-
-
-
-
+def check_file(path):
+    # "Detector_state.json"
+    if os.path.isfile(path) == False:
+        pass
 
 
 @app.put("/api/detector/initialized")
-async def initialized():
-    
+async def initialized(detector: Detector):
+    if os.path.isfile("Detector_state.json"):
+        with open('Detector_state.json') as file:
+            pass
 
     return {"message": "Hello World"}
 
